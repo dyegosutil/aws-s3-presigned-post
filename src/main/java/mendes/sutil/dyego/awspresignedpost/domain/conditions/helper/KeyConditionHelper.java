63ceeae3f08ad5398e8c8510dfa54b4daf3d6ed6
Dyego <dyego.sutil@gmail.com>
2022-09-25 12:35:13 +0200

    Add bucket as mandatory field

--- src/main/java/mendes/sutil/dyego/awspresignedpost/domain/conditions/helper/KeyConditionHelper.java
+++ src/main/java/mendes/sutil/dyego/awspresignedpost/domain/conditions/helper/KeyConditionHelper.java
@@ -1,4 +1,7 @@
-package mendes.sutil.dyego.awspresignedpost.domain.conditions;
+package mendes.sutil.dyego.awspresignedpost.domain.conditions.helper;
+
+import mendes.sutil.dyego.awspresignedpost.domain.conditions.ExactKeyCondition;
+import mendes.sutil.dyego.awspresignedpost.domain.conditions.KeyCondition;
 
 /**
  * Provides helper methods to create instances of {@link KeyCondition}
