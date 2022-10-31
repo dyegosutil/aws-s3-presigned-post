954fdcd15b7850b9f271296ed0408ba70e42a04c
Dyego <dyego.sutil@gmail.com>
2022-10-31 12:47:14 +0100

    add import

--- src/test/java/mendes/sutil/dyego/awspresignedpost/integrationtests/FreeTextConditionIntegrationTest.java
+++ src/test/java/mendes/sutil/dyego/awspresignedpost/integrationtests/FreeTextConditionIntegrationTest.java
@@ -10,6 +10,7 @@ import okhttp3.RequestBody;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Disabled;
 
 import java.time.Clock;
 import java.time.ZoneOffset;
