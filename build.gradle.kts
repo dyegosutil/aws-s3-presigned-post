7dc34ab72c06c58de2116f72fd0a17f8bea113d9
Dyego Sutil Mendes <dyego.sutil@gmail.com>
2024-07-07 18:18:36 +0200

    Increment version to 0.1.0-beta.7 (#309)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import java.net.URI
 
 group = "io.github.dyegosutil"
-version = "0.1.0-beta.6"
+version = "0.1.0-beta.7"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 val jupiterVersion = "5.10.3"
